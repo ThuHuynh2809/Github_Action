0afd2c0bb60a2884ab47022857db05f8584f4fa4
ThuHuynh2809 <huynhminhthu860@gmail.com>
2024-04-15 02:58:49 +0700

    up9

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 type Params = {
   params: {
     slug: string
@@ -10,24 +8,9 @@ export async function generateMetadata({ params }: Params) {
   return { title: `Post: ${params.slug}` }
 }
 
-const Page: React.FC<Params> = ({ params }) => {
-  return (
-    <div>
-      <h1>Slug: {params.slug}</h1>
-      <p>Huỳnh Minh Thủ</p>
-      <p>123</p>
-    </div>
-  );
-}
-
-export default Page;
-
-// Thêm hàm generateStaticParams() vào cuối file
-export async function generateStaticParams() {
-  // Thực hiện logic để tạo ra các tham số tĩnh
-  return [
-    { params: { slug: 'Huỳnh Minh Thủ' } },
-    { params: { slug: '123' } },
-    // Thêm các tham số khác nếu cần
-  ];
+export default function Page({ params }: Params) {
+  return <div>
+    <h1>Slug: {params.slug}</h1>
+    <p>Huỳnh Minh Thủ</p><p>123</p>
+  </div>
 }
