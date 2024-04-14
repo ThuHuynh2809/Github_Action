7fc80c1b0090810f1351d2069d976862be6122e8
ThuHuynh2809 <huynhminhthu860@gmail.com>
2024-04-15 02:44:39 +0700

    up4

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,16 +1,25 @@
+import React from 'react';
+
 type Params = {
   params: {
     slug: string
   }
 }
 
-export async function generateMetadata({ params }: Params) {
-  return { title: `Post: ${params.slug}` }
+// Hàm generateMetadata nhận đối số là một đối tượng Params và trả về một promise chứa một đối tượng metadata
+export async function generateMetadata({ params }: Params): Promise<{ title: string }> {
+  return { title: `Post: ${params.slug}` };
 }
 
-export default function Page({ params }: Params) {
-  return <div>
-    <h1>Slug: {params.slug}</h1>
-    <p>Huỳnh Minh Thủ</p><p>123</p>
-  </div>
+// Component Page nhận một đối tượng Params và hiển thị nội dung dựa trên slug trong params
+const Page: React.FC<Params> = ({ params }) => {
+  return (
+    <div>
+      <h1>Slug: {params.slug}</h1>
+      <p>Huỳnh Minh Thủ</p>
+      <p>123</p>
+    </div>
+  );
 }
+
+export default Page;
